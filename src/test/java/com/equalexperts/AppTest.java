0ccc13bcf1c900703a52328973fcdcb587d8f98d
Krishnan Mani <kmani@equalexperts.com>
2015-07-17 17:38:25 +0530

    Added a simple test to check for blog author

---
+++ src/test/java/com/equalexperts/AppTest.java
@@ -0,0 +1,41 @@
+package com.equalexperts;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    public void testBlogPageStructure() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.equalexperts.com/blog/");
+
+        assertEquals(driver.findElement(By.className("author-name")).getText(), "Louis Abel");
+        driver.close();
+    }
+}
