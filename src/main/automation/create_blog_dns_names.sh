9546e8b5abb2558024c71576b1732828ed94121a
Krishnan Mani <kmani@equalexperts.com>
2015-07-18 08:50:22 +0530

    Corrections for the CNAME

--- src/main/automation/create_blog_dns_names.sh
+++ src/main/automation/create_blog_dns_names.sh
@@ -20,7 +20,7 @@ do
     echo "For user ${username}"
     export BUCKET_NAME="${username}.${SUB_DOMAIN}"
     export FQDN="${BUCKET_NAME}.equalexperts.in"
-    export POINTS_TO="${BUCKET_NAME}.s3-website-${REGION_NAME}.amazonaws.com"
+    export POINTS_TO="${FQDN}.s3-website-${REGION_NAME}.amazonaws.com"
     cp cname_resource_record_set.json.template cname_resource_record_set.json
     sed -i '' "s/FQDN/${FQDN}/" cname_resource_record_set.json
     sed -i '' "s/POINTS_TO/${POINTS_TO}/" cname_resource_record_set.json
